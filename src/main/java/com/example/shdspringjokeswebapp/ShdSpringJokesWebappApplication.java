6a92f8cbe18f5ea12ad0489a65c365164163d1b5
shadow2105 <ashwinrajput66@gmail.com>
2023-02-19 13:54:33 -0500

    Initial Project Add. Close #1

---
+++ src/main/java/com/example/shdspringjokeswebapp/ShdSpringJokesWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.shdspringjokeswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShdSpringJokesWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShdSpringJokesWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shdspringjokeswebapp/ShdSpringJokesWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shdspringjokeswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShdSpringJokesWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
